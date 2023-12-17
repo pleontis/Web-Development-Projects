73d139880b17e64ab76518c992296c40849e3a02
Panagiotis Leontis <67234862+pleontis@users.noreply.github.com>
2023-12-17 20:47:56 +0200

    Add files via upload

---
+++ Dice Game/index.js
@@ -0,0 +1,24 @@
+var randomNumber1= Math.floor(Math.random()*6)+1;
+
+var randomDiceImg="dice"+randomNumber1+".png";
+
+var randomImgSource="images/"+randomDiceImg;
+
+document.querySelectorAll("img")[0].setAttribute("src",randomImgSource);
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+
+var randomImgSource2="images/dice"+ randomNumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImgSource2);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="💥 Player 1 Wins !"
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="💥 Player 2 Wins !"
+}
+else{
+    document.querySelector("h1").innerHTML="Draw"
+}
